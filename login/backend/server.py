d93332e185be67054cc45b81e31a898749e93357
debaditya4421 <103170797+debaditya4421@users.noreply.github.com>
2023-03-23 22:29:07 +0530

    1

--- login/backend/server.py
+++ login/backend/server.py
@@ -1,6 +1,7 @@
 # Import flask and datetime module for showing date and time
-# import MySQLdb
-from flask import Flask, Response, request
+import MySQLdb
+from flask_mysqldb import MySQL
+from flask import Flask, Response, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 import requests
@@ -9,25 +10,36 @@ x = datetime.datetime.now()
 
 # Initializing flask app
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/user_signup'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'user_signup'
 db = SQLAlchemy(app)
+# mysql = MySQL(app)
 
 class Users_data(db.Model):
 	sno = db.Column(db.Integer, primary_key=True)
 	Name = db.Column(db.String(30), unique=True , nullable=False)
 	age = db.Column(db.Integer, unique=True , nullable=False)
 
+	# def __init__(self,Name,age):
+	# 	self.Name = Name
+	# 	self.age = age
+
 # Route for seeing a data
-@app.route('/submit' , methods= ['GET' , 'POST'] )
+@app.route('/submit' , methods= ['GET','POST'] )
 def submit_data():
 	if(request.method == 'POST'):
+		print("HI")
 		name = request.form.get('fname')
 		age = request.form.get('age')
 		entry = Users_data(Name = name , age=age)
 		db.session.add(entry)
 		db.session.commit()
+	return redirect("http://localhost:3000/")
 		
-	
 # Running app
 if __name__ == '__main__':
 	app.run(debug=True)
--- login/frontend/bas_app/src/App.js
+++ login/frontend/bas_app/src/App.js
@@ -1,18 +1,39 @@
 // Importing modules
 import React, { useState, useEffect } from "react";
+import {BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
 import "./App.css";
 
 function App() {
 
 	return (
-		<div className="App">
-			<form action="/submit" method="POST">
+	
+		<Routes>
+			<Route
+            exact
+            path="/"
+            element={     
+				<div className="App">
+			<form action="/submit" method="post">
 				<label for="fname">Name:</label>
 				<input type="text" id="fname" name="fname" />
 				<label for="age">Age:</label>
 				<input type="text" id="lname" name="age" />
+				<input type="submit"></input>
 			</form>
 		</div>
+			      }
+          />
+			<Route
+            exact
+            path="/submit"
+            element={     
+				<div>
+					Hello World
+				</div>
+			      }
+          />
+		</Routes>
+		
 	);
 }
 
